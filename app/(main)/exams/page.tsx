9e3c465f9a1604b2dd97e125b7353fc901a407c0
hessam <amirhessam.dev@gmail.com>
2025-07-15 16:42:23 +0330

    add exam mock data, add exam card, and exam pge listing

--- app/(main)/exams/page.tsx
+++ app/(main)/exams/page.tsx
@@ -1,9 +1,14 @@
+import exams from "@/lib/mocks/exams";
+
+import ExamCard from "@/components/features/exam-list/ExamCard";
+
 export default function Exams() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
-      <div className="binsa-container py-8">
-        <h1 className="text-4xl font-bold">Exams</h1>
-        <p className="text-lg">View your exams and results.</p>
+      <div className="binsa-container grid grid-cols-1 gap-4 py-8 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
+        {exams.map((exam) => (
+          <ExamCard key={exam.id} exam={exam} />
+        ))}
       </div>
     </div>
   );
---
+++ components/features/exam-list/ExamCard.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
+import { Exam } from "@/lib/mocks/exams";
+
+export default function ExamCard({ exam }: { exam: Exam }) {
+  return (
+    <div className="card card-border bg-base-100 col-span-1">
+      <div className="card-body p-2">
+        <div className="flex aspect-square w-full flex-col items-start justify-start rounded-xl bg-amber-100 p-3">
+          <div className="badge mb-2 rounded-lg border-transparent bg-white/70">
+            <span>{exam.type === "ai" ? "AI Generated" : "Manual"}</span>
+          </div>
+          <h2 className="card-title grow items-start justify-start text-2xl">
+            {exam.name}
+          </h2>
+          <div className="flex w-full items-center justify-between">
+            <span>{exam.questions_count} Questions</span>
+            <span>{exam.estimated_time} Minutes</span>
+          </div>
+        </div>
+        <div className="card-actions justify-end">
+          <Link
+            href={`/exams/${exam.id}`}
+            className="btn btn-neutral btn-block rounded-xl"
+          >
+            Take Exam
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
