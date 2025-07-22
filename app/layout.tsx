cd07b00802b072ba84ba157319c2f2d31a6a9e02
hessam <amirhessam.dev@gmail.com>
2025-07-22 19:59:01 +0330

    move QueryClientProvider and AuthProvider to the client side

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-import React from "react";
 import type { Metadata } from "next";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { AuthProvider } from "@/context/AuthContext";
+import ClientProviders from "@/components/ClientProviders";
 
 import { Rubik } from "next/font/google";
 import "./globals.css";
@@ -22,14 +19,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = React.useState(() => new QueryClient());
   return (
     <html lang="en">
       <body className={`${rubikSans.className} antialiased`}>
-        <QueryClientProvider client={queryClient}>
-          <AuthProvider>{children}</AuthProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
   );
---
+++ components/ClientProviders.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthProvider } from "@/context/AuthContext";
+
+export default function ClientProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{children}</AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
