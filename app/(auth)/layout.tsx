b640a506ecdb18cafed2b83c9ab89e1cc54e124d
hessam <amirhessam.dev@gmail.com>
2025-07-22 19:02:39 +0330

    add base layout for auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,10 +4,15 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid grid-cols-2 gap-4 p-8 min-h-screen">
-      <section className="col-span-1">{children}</section>
-      <section className="col-span-1 bg-base-200 rounded-2xl p-4">
-        Side Pane
+    <div className="grid min-h-screen gap-4 p-8 md:grid-cols-2">
+      <section className="col-span-1 flex items-center justify-center">
+        {children}
+      </section>
+      <section className="bg-base-200 col-span-1 hidden items-center justify-center rounded-2xl p-4 md:flex">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold">Side Pane</h2>
+          <p>This area can contain promotional content or images.</p>
+        </div>
       </section>
     </div>
   );
